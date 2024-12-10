880313e07a859b03209c7f2fa2e39a4c004b972f
Bryce Cain <cainbryce208@gmail.com>
2024-12-09 20:18:04 -0700

    Initial commit

---
+++ index.ts
@@ -0,0 +1,19 @@
+/**
+ * @description This file is just the main entrance to your app. Use it to initialize and run everything. Shouldn't be used to define, declare, or otherwise make functionality
+ */
+
+/** 
+ * @example 
+ * import { blahEcho } from "dc-blah"
+import blah from 'dc-blah';
+ * 
+ * blahEcho()
+ */
+
+import { blahEcho } from "dc-blah"
+
+console.log(
+    blahEcho()
+)
+
+/* this is the method from src/blah/index.ts! */
---
+++ src/blah/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @author @cainba
+ * @description defines methods: blahEcho
+ * @exports blahEcho
+ */
+
+export function blahEcho(): string {
+    return `Hellooo ${import.meta.dir}`
+}
